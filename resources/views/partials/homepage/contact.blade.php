fa0b2f35faff2f0ce40d2bee6ddda5b807faa356
luiscpiedrahita <luis1420@gmail.com>
2026-02-07 17:54:50 -0500

    Migrated homepage contact partial to Laravel blade

--- resources/views/partials/homepage/contact.blade.php
+++ resources/views/partials/homepage/contact.blade.php
@@ -1,19 +1,20 @@
-
 <a class="anchor" id="contact"></a>
 <section class="contact container-fluid">
-    <div class="container">
+  <div class="container">
     <div class="row g-0 sent-thanks alert alert-success">
-    <div class="col-lg-4 offset-lg-4">
+      <div class="col-lg-4 offset-lg-4">
         <h2 class="display-1"><strong>Thank you for your message!</strong></h2>
+      </div>
+      <div
+        class="col-lg-2 svg-icon--title"
+        style="
+          background-image: url(/app/themes/helios/dist/homepage/icons/mail.svg);
+        "
+      ></div>
     </div>
-    <div class="col-lg-2 svg-icon--title" style="
-                    background-image: url(/app/themes/helios/dist/homepage/icons/mail.svg);
-                  ">
-    </div>
-    </div>';
-$out .= do_shortcode('[contact-form-7 id="e7d6778" title="Contact"]');
-
-$out .= '<div class="wpcf7-spinner-wrapper"><div class="wpcf7-spinner">
-</div>
-</div>
+    {!! do_shortcode('[contact-form-7 id="e7d6778" title="Contact"]') !!}
+  </div>
+  <div class="wpcf7-spinner-wrapper">
+    <div class="wpcf7-spinner"></div>
+  </div>
 </section>
